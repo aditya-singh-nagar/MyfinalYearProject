088993f9cb2e7481f494d4ed52c35fc274d76292
Aditya-singh12345 <hritikkaushik2003@gmail.com>
2026-07-05 15:12:41 +0530

    add a test controller

--- src/main/java/mpack/configurations/SecurityConfiguration.java
+++ src/main/java/mpack/configurations/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration {
 
 	    CorsConfiguration configuration = new CorsConfiguration();
 
-	    configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500" ,  "https://your-vercel-app.vercel.app"));
+	    configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500" ,  "https://your-vercel-app.vercel.app" ,  "https://your-netlify-app.netlify.app"));
 
 	    configuration.setAllowedMethods(List.of(
 	            "GET",
