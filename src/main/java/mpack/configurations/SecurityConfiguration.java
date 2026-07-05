7c8d90a342aeaa9fe99cfe09396938764f2e3271
Aditya-singh12345 <hritikkaushik2003@gmail.com>
2026-07-05 09:58:04 +0530

    add the vercel link in cors and add the dialect in appl prop

--- src/main/java/mpack/configurations/SecurityConfiguration.java
+++ src/main/java/mpack/configurations/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration {
 
 	    CorsConfiguration configuration = new CorsConfiguration();
 
-	    configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
+	    configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500" ,  "https://your-vercel-app.vercel.app"));
 
 	    configuration.setAllowedMethods(List.of(
 	            "GET",
